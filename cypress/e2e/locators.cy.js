dab19bf61ce8db71142ece8df33bebd5448a2099
ahmethasanmesut <mesutyavuz34@gmail.com>
2023-05-07 15:10:37 +0100

    new locators

--- cypress/e2e/locators.cy.js
+++ cypress/e2e/locators.cy.js
@@ -12,10 +12,20 @@ describe('Context: My First Tests',() =>{
         //every statement creates an object to be interacted,and next command makes operation to the object at the previous statement
         //attribute name and value
         cy.get("[type='text']").clear();//clear what is typed
+
+        //tagName
         cy.get("input").each((item,index,list)=>{
             //assert the length of the list is 2
             expect(list).to.have.length(2);
             expect(item).to.have.attr("type");
         })
+        //by attribute name
+        cy.get('[type]');
+        //by className
+        cy.get('.btn.btn-primary');
+        //by id
+        cy.get('#wooden_spoon');
+        //if I want to use text: no xpath in cypress,but it is still possible to use text
+        cy.get('button').should('contain','Login').click();
     })
 })
